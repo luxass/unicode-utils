0774b8649d22057faac27ed1b7fbea2750f877c4
Lucas <lucasnrgaard@gmail.com>
2025-04-28 07:22:56 +0200

    fix: add missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
 export { UNICODE_VERSIONS, UNICODE_VERSIONS_WITH_UCD } from "./constants";
-export { isCommentLine, parseDataFileHeading, RawDataFile } from "./data-files";
+export {
+  isCommentLine,
+  isEmptyLine,
+  isMissingAnnotation,
+  type MissingAnnotation,
+  parseDataFileHeading,
+  parseMissingAnnotation,
+  RawDataFile,
+} from "./data-files";
