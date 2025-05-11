71c4be1878bee110586f3e582c1fb70ddd1216a7
Lucas <lucasnrgaard@gmail.com>
2025-05-11 06:03:13 +0200

    feat: enhance section tests and add parseSections functionality

--- test/data-files/sections.test.ts
+++ test/data-files/sections.test.ts
@@ -1,12 +1,33 @@
 import { describe, expect, it } from "vitest";
-import { hasSections } from "../../src/data-files";
+import { hasSections, parseSections, RawDataFile } from "../../src/data-files";
 import { dataFileFixture } from "../__utils";
 
 describe("hasSections", () => {
   it.each([
+    [{ version: "4.1.0", file: "ArabicShaping.txt", expected: true }],
+    [{ version: "4.1.0", file: "BidiMirroring.txt", expected: false }],
+    [{ version: "4.1.0", file: "CaseFolding.txt", expected: false }],
     [{ version: "11.0.0", file: "PropertyAliases.txt", expected: true }],
+    [{ version: "16.0.0", file: "PropertyAliases.txt", expected: true }],
+    [{ version: "16.0.0", file: "UnicodeData.txt", expected: false }],
   ])("hasSections($version - $file)", ({ file, version, expected }) => {
-    const dataFile = dataFileFixture(version, file);
-    expect(hasSections(dataFile)).toBe(expected);
+    const content = dataFileFixture(version, file);
+    const datafile = new RawDataFile(content, file.replace(/\.txt$/, ""));
+    expect(hasSections(datafile.content)).toBe(expected);
+  });
+});
+
+describe("parseSections", () => {
+  it.each([
+    [{ version: "4.1.0", file: "ArabicShaping.txt", expected: 4 }],
+    [{ version: "4.1.0", file: "BidiMirroring.txt", expected: 0 }],
+    [{ version: "4.1.0", file: "CaseFolding.txt", expected: 0 }],
+    [{ version: "11.0.0", file: "PropertyAliases.txt", expected: 6 }],
+    [{ version: "16.0.0", file: "PropertyAliases.txt", expected: 6 }],
+    [{ version: "16.0.0", file: "UnicodeData.txt", expected: 0 }],
+  ])("hasSections($version - $file)", ({ file, version, expected }) => {
+    const content = dataFileFixture(version, file);
+    const datafile = new RawDataFile(content, file.replace(/\.txt$/, ""));
+    expect(parseSections(datafile.content).size).toBe(expected);
   });
 });
