d839f9626856ec8602c5a7aae4d358147c69490d
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-05-15 19:55:11 +0200

    feat: simplify UCD file handling and update dependencies

--- test/__utils.ts
+++ test/__utils.ts
@@ -1,4 +1,5 @@
 import { readFileSync } from "node:fs";
+import { glob } from "node:fs/promises";
 import { join } from "node:path";
 
 export function dataFileFixture(version: string, file: string) {
@@ -13,4 +14,33 @@ export function ucdFiles(version: string, file: string) {
     join(__dirname, "../ucd-files", `v${version}`, file),
     "utf-8",
   );
+}
+
+export async function mapUCDFiles(version: string) {
+  const files: string[] = [];
+  const globbedFiles = glob([
+    "**/*.txt",
+  ], {
+    cwd: join(__dirname, "../ucd-files", `v${version}`),
+    exclude(fileName) {
+      // exclude files that end with .comments.txt
+      return fileName.endsWith(".comments.txt");
+    },
+  });
+
+  for await (const file of globbedFiles) {
+    files.push(file);
+  }
+
+  return {
+    files,
+    file(file: string) {
+      files.splice(files.indexOf(file), 1);
+
+      return readFileSync(
+        join(__dirname, "../ucd-files", `v${version}`, file),
+        "utf-8",
+      );
+    },
+  };
 }
--- test/heading-inferrence/inferrence-v12.test.ts
+++ test/heading-inferrence/inferrence-v12.test.ts
@@ -1,54 +1,60 @@
 import { describe, expect, it } from "vitest";
 import { inferHeading } from "../../src/heading-inference";
-import { ucdFiles } from "../__utils";
+import { mapUCDFiles } from "../__utils";
+
+const ucdFiles = await mapUCDFiles("12.0.0");
 
 describe("heading inferrence v12", async () => {
   it("inferHeading(ArabicShaping)", () => {
-    const content = ucdFiles("12.0.0", "ArabicShaping.txt");
-    const expected = ucdFiles("12.0.0", "ArabicShaping.comments.txt");
+    const content = ucdFiles.file("ArabicShaping.txt");
+    const expected = ucdFiles.file("ArabicShaping.comments.txt");
 
     expect(inferHeading(content)).toBe(expected);
   });
 
   it("inferHeading(BidiBrackets)", () => {
-    const content = ucdFiles("12.0.0", "BidiBrackets.txt");
-    const expected = ucdFiles("12.0.0", "BidiBrackets.comments.txt");
+    const content = ucdFiles.file("BidiBrackets.txt");
+    const expected = ucdFiles.file("BidiBrackets.comments.txt");
 
     expect(inferHeading(content)).toBe(expected);
   });
 
   it("inferHeading(BidiCharacterTest)", () => {
-    const content = ucdFiles("12.0.0", "BidiCharacterTest.txt");
-    const expected = ucdFiles("12.0.0", "BidiCharacterTest.comments.txt");
+    const content = ucdFiles.file("BidiCharacterTest.txt");
+    const expected = ucdFiles.file("BidiCharacterTest.comments.txt");
 
     expect(inferHeading(content)).toBe(expected);
   });
 
   it("inferHeading(BidiMirroring)", () => {
-    const content = ucdFiles("12.0.0", "BidiMirroring.txt");
-    const expected = ucdFiles("12.0.0", "BidiMirroring.comments.txt");
+    const content = ucdFiles.file("BidiMirroring.txt");
+    const expected = ucdFiles.file("BidiMirroring.comments.txt");
 
     expect(inferHeading(content)).toBe(expected);
   });
 
   it("inferHeading(BidiTest)", () => {
-    const content = ucdFiles("12.0.0", "BidiTest.txt");
-    const expected = ucdFiles("12.0.0", "BidiTest.comments.txt");
+    const content = ucdFiles.file("BidiTest.txt");
+    const expected = ucdFiles.file("BidiTest.comments.txt");
 
     expect(inferHeading(content)).toBe(expected);
   });
 
   it("inferHeading(Blocks)", () => {
-    const content = ucdFiles("12.0.0", "Blocks.txt");
-    const expected = ucdFiles("12.0.0", "Blocks.comments.txt");
+    const content = ucdFiles.file("Blocks.txt");
+    const expected = ucdFiles.file("Blocks.comments.txt");
 
     expect(inferHeading(content)).toBe(expected);
   });
 
   it("inferHeading(CaseFolding)", () => {
-    const content = ucdFiles("12.0.0", "CaseFolding.txt");
-    const expected = ucdFiles("12.0.0", "CaseFolding.comments.txt");
+    const content = ucdFiles.file("CaseFolding.txt");
+    const expected = ucdFiles.file("CaseFolding.comments.txt");
 
     expect(inferHeading(content)).toBe(expected);
   });
+});
+
+it("ensure that all files have been tested", () => {
+  expect(ucdFiles.files.length).toBe(0);
 });
