aa433f3990b61e3ffee7a5211b67335dd0819813
Lucas <lucasnrgaard@gmail.com>
2025-05-17 08:24:06 +0200

    fix

--- test/heading-inferrence/inference-v16.test.ts
+++ test/heading-inferrence/inference-v16.test.ts
@@ -294,7 +294,7 @@ describe("heading inference v16.0.0", async () => {
     });
   });
 
-  describe.only("extracted files", () => {
+  describe("extracted files", () => {
     it("inferHeading(DerivedBidiClass)", () => {
       const content = ucdFiles.file("extracted/DerivedBidiClass.txt");
       const expected = ucdFiles.expected("extracted/DerivedBidiClass.comments.txt");
