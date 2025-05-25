94bcee987f1278a13cb6a06fd9be1c1056fe0798
Lucas <lucasnrgaard@gmail.com>
2025-05-25 17:20:05 +0200

    fix

--- test/inference/heading-v16.test.ts
+++ test/inference/heading-v16.test.ts
@@ -11,7 +11,7 @@ import { mapUCDFiles } from "../__utils";
 
 const ucdFiles = await mapUCDFiles("v16.0.0");
 
-describe.todo("heading inference v16", async () => {
+describe("heading inference v16", async () => {
   it("inferHeadingFromAST(ArabicShaping.txt)", () => {
     const content = ucdFiles.file("ArabicShaping.txt");
     const expected = ucdFiles.expected("/ArabicShaping.comments.txt");
