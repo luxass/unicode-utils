456847be793a60e8896e5310de6e8421a47a4e5c
Lucas <lucasnrgaard@gmail.com>
2025-07-01 06:34:36 +0200

    refactor: remove worker
    Since the ucdjs api now have all the things implemented that used to only be inside the unicode-api worker, we can now get rid of the unicode-api worker.

--- api/src/types.ts
+++
@@ -1,8 +0,0 @@
-import type { z } from "zod";
-import type { ApiErrorSchema } from "./schemas";
-
-export interface HonoEnv {
-  Bindings: CloudflareBindings;
-}
-
-export type ApiError = z.infer<typeof ApiErrorSchema>;
