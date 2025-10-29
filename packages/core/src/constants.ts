6d1ce18c206482024be54ecb38d5feffe1cc578f
Lucas <lucasnrgaard@gmail.com>
2025-10-29 20:21:53 +0100

    chore: rename

--- packages/core/src/constants.ts
+++ packages/core/src/constants.ts
@@ -4,10 +4,10 @@
  * This maintains backwards compatibility while allowing the metadata
  * to be imported independently via @unicode-utils/metadata
  */
+
 export {
   UNICODE_DRAFT_VERSION,
   UNICODE_STABLE_VERSION,
   UNICODE_VERSION_METADATA,
 } from "@unicode-utils/metadata";
-
 export type { UnicodeVersionMetadata } from "@unicode-utils/metadata";
