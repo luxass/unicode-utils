2b647921a5677fc6bdb56b84bade0fb7a1017213
Lucas <lucasnrgaard@gmail.com>
2026-03-31 17:22:47 +0200

    fix: correct data-file entry point in tsdown config and package.json
    The build config referenced ./src/datafile.ts but the file is ./src/data-file.ts.

--- packages/parser/tsdown.config.ts
+++ packages/parser/tsdown.config.ts
@@ -1,5 +1,5 @@
 import { createTsdownConfig } from "@unicode-utils-tooling/tsdown-config";
 
 export default createTsdownConfig({
-  entry: ["./src/index.ts", "./src/datafile.ts", "./src/line-helpers.ts"],
+  entry: ["./src/index.ts", "./src/data-file.ts", "./src/line-helpers.ts"],
 });
